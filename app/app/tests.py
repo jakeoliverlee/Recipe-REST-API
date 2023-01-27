76e25524c0e63e1cbb2c9bdaff7b1014fd6904b8
Jake <jakeoliverlee@gmail.com>
2023-01-27 15:39:07 +0000

    remove django test functions

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,17 +1,4 @@
 from django.test import SimpleTestCase
-from app import calc
-# Sample Test case
-
-class CalcTests(SimpleTestCase):
-    def test_add_numbers(self):
-        res = calc.add(5, 6)
-
-        self.assertEqual(res, 11)
-
-class CalcTests(SimpleTestCase):
-    def test_subtract_numbers(self):
-        res = calc.substract(10, 3)
-        self.assertEqual(res, 7)
 
 
 
