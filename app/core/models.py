626f538950cd4cee7b0ae4ce1bf2b5f57ebbc93e
Jake <jakeoliverlee@gmail.com>
2023-02-02 10:03:59 +0000

    Implementing user model

---
+++ app/core/models.py
@@ -0,0 +1,30 @@
+"""
+database models
+"""
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_field):
+        """Create, save and return new user"""
+        user = self.model(email=email, **extra_field)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+class User (AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    # Determines if user can login to django admin
+    is_staff = models.BooleanField(default=True)
+
+
+    USERNAME_FIELD = 'email'
